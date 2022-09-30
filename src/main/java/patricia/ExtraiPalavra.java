58d7fd883964c76289e8003b2f6eb39fdb2fded8
Vinícius Ferreira Pinheiro <99216953+vini-2205@users.noreply.github.com>
2022-09-29 22:06:09 -0300

    Update ExtraiPalavra.java

--- src/main/java/patricia/ExtraiPalavra.java
+++ src/main/java/patricia/ExtraiPalavra.java
@@ -1,13 +1,12 @@
-package patricia;
 
 import java.util.StringTokenizer;
 import java.io.*;
 public class ExtraiPalavra {
-  private BufferedReader arqDelim, arqTxt;
+  private BufferedReader arqTxt;
   private StringTokenizer palavras;
-  private String delimitadores;
-  private int linha=1;
-  private int coluna=1;
+  //private String delimitadores;
+  private int linha=0;
+  private int coluna=0;
 
     public int getLinha() {
         return linha;
@@ -17,11 +16,10 @@ public class ExtraiPalavra {
         return coluna;
     }
 
-  
-  
-  public ExtraiPalavra (String nomeArqDelim, String nomeArqTxt) 
+
+  public ExtraiPalavra (String nomeArqTxt) 
   throws Exception {
-    this.arqDelim = new BufferedReader (new FileReader (nomeArqDelim));
+    //this.arqDelim = new BufferedReader (new FileReader (nomeArqDelim));
     this.arqTxt = new BufferedReader (new FileReader (nomeArqTxt));
     // @{\it Os delimitadores devem estar juntos em uma \'unica linha do arquivo}@ 
     //this.delimitadores = arqDelim.readLine() + "\r\n"; 
@@ -29,18 +27,18 @@ public class ExtraiPalavra {
   }  
   public String proximaPalavra () throws Exception{
     if (palavras == null || !palavras.hasMoreTokens()) {
-        coluna=1;
+        coluna=0;
         linha++;
       String linha = arqTxt.readLine();
       if (linha == null) return null; 
-      this.palavras = new StringTokenizer (linha, " /()-,.?1234567890");
+      this.palavras = new StringTokenizer (linha, " /()-,.?1234567890*");
       if (!palavras.hasMoreTokens()) return ""; // @{\it ignora delimitadores}@
     }
     coluna++;
     return this.palavras.nextToken ();
   }  
   public void fecharArquivos () throws Exception {
-    this.arqDelim.close(); 
+    //this.arqDelim.close(); 
     this.arqTxt.close();
   }
 }
